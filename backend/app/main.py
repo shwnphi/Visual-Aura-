61f16390ddb8504502570ec8311cafd80780a92b
Shawne Laquindanum <shawnebenedict@Outlook.com>
2025-11-08 19:45:02 -0500

    finished keywords, transcript, and controls components

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,17 +27,17 @@ class Transcript(BaseModel):
 ## Analyzes the transcription
 @app.post("/process_text")
 def transcribe(input: Transcript):
-
     data = {}
+    keywords_list = []
     text = input.text
 
     response = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
-        {"role": "system", "content": "Respond in this format: number,emotion"},
+        {"role": "system", "content": "Respond in this format: number,emotion, keyword 1, keyword 2, keyword 3"},
         {"role": "user", "content": f"""Analyze this {text}, 
          respond with the sentiment from -1(negative) to 1(positive), 
-         the primary emotion of {text}. """}
+         the primary emotion of {text}, and 3 Keywords """}
         ],
         temperature = 0 
 )   
@@ -47,6 +47,13 @@ def transcribe(input: Transcript):
 
     data["sentiment"] = float(split_answer[0].strip())
     data["emotion"] = split_answer[1].strip()
+
+    for i in range(2, len(split_answer)):
+        keywords_list.append(split_answer[i].strip())
+
+    data["keywords"] = keywords_list
+
+
 
     return data
 
